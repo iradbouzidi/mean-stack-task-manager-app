cda2eb2bd9f316a7a8df8bad0e2a4993814f0e28
iradbouzidi <irad.bouzidi@esprit.tn>
2020-12-08 14:11:50 +0100

    init

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { WebRequestService } from '../services/web-request.service';
+import { Task } from '../models/task.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(private webReqService: WebRequestService) { }
+
+  // region LIST CRUD
+  getLists() {
+    return this.webReqService.get('lists');
+  }
+
+  createList(title: string) {
+    // We want to send a web request to create a list
+    return this.webReqService.post('lists', { title });
+  }
+
+  updateList(id: string, title: string) {
+    // We want to send a web request to update a list
+    return this.webReqService.patch(`lists/${id}`, { title });
+  }
+
+  deleteList(id: string) {
+    return this.webReqService.delete(`lists/${id}`);
+  }
+  // endregion
+
+  // region TASK CRUD
+  getTasks(listId: string) {
+    return this.webReqService.get(`lists/${listId}/tasks`);
+  }
+
+  createTask(title: string, listId: string) {
+    // We want to send a web request to create a task
+    return this.webReqService.post(`lists/${listId}/tasks`, { title });
+  }
+
+  deleteTask(listId: string, taskId: string) {
+    return this.webReqService.delete(`lists/${listId}/tasks/${taskId}`);
+  }
+
+  updateTask(listId: string, taskId: string, title: string) {
+    // We want to send a web request to update a list
+    return this.webReqService.patch(`lists/${listId}/tasks/${taskId}`, { title });
+  }
+
+  complete(task: Task) {
+    return this.webReqService.patch(`lists/${task._listId}/tasks/${task._id}`, {
+      completed: !task.completed
+    });
+  }
+
+  // endregion
+
+}
